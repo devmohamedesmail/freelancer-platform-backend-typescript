d74689eccbfbbc08e6297f6baf85c03a0cfad9cb
Mohamed Esmail <mohamedesmail@Mohameds-MacBook-Pro.local>
2025-12-28 23:54:25 +0400

    fix the error of auth

--- src/routes/reviews/routes.ts
+++ src/routes/reviews/routes.ts
@@ -11,45 +11,45 @@ const router = express.Router();
  * Get reviews by user ID
  * router.get('/user/:user_id', ReviewController.getByUser);
  */
-router.get('/', ReviewController.index);
+// router.get('/', ReviewController.index);
 
 /**
  * Get reviews by store ID
  * router.get('/store/:store_id', ReviewController.getByStore);
  * 
  */
-router.get('/store/:store_id', ReviewController.getByStore);
+// router.get('/store/:store_id', ReviewController.getByStore);
 
 /**
  * Get reviews by user ID
  * router.get('/user/:user_id', ReviewController.getByUser);
  */
-router.get('/user/:user_id', ReviewController.getByUser);
+// router.get('/user/:user_id', ReviewController.getByUser);
 
 /**
  * Get single review by ID
  * router.get('/:id', ReviewController.find);
  */
-router.get('/:id', ReviewController.find);
+// router.get('/:id', ReviewController.find);
 
 /**
  * Create review
  * 
  */
-router.post('/create', ReviewController.create);
+// router.post('/create', ReviewController.create);
 
 /**
  * Update review
  * router.put('/:id', ReviewController.update);
  */
-router.put('/:id', ReviewController.update);
+// router.put('/:id', ReviewController.update);
 
 /**
  * Delete review
  * router.delete('/:id', ReviewController.destroy); 
  * 
  */
-router.delete('/:id', ReviewController.destroy);
+// router.delete('/:id', ReviewController.destroy);
 
 
 export { router as reviewsRoutes };
